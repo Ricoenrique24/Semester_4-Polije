fcc8940369017c8a4c54ed0b0f5aa1d4aae26663
Rico Enrique <terongraos@gmail.com>
2023-03-08 10:34:38 +0700

    Acara 16 has been Slain!
    Mengerjakan BKPM Acara 16!

---
+++ Mobile Apps/Tugas/Acara 16/telegram_apps/lib/Tugas12/Drawwer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DrawerScreen extends StatefulWidget {
+  @override
+  _DrawerScreenState createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen>{
+  @override
+  Widget build(BuildContext context){
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("Mochammad Enrique Lazuardi Ramadany"), 
+            currentAccountPicture: CircleAvatar(backgroundImage: AssetImage("assets/img/2.jpg")
+            ),
+            accountEmail: Text("merico@gmail.com")
+            ),
+            DrawerListTile(
+              iconData: Icons.group,
+              title: "New Group",
+              onTilePressed: (){}
+            )
+        ]
+      )
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget{
+    final IconData? iconData;
+    final String? title;
+    final VoidCallback? onTilePressed;
+
+    const DrawerListTile({Key? key, this.iconData, this.title, this.onTilePressed})
+    :super(key: key);
+
+    @override
+    Widget build(BuildContext context){
+      return ListTile(
+        onTap: onTilePressed,
+        dense: true,
+        leading: Icon(iconData),
+        title: Text(title.toString(), style: TextStyle(fontSize: 16),),
+      );
+    }
+}
